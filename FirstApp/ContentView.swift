a8e4aa64cb8ddac73bfeaf3653f22415450b6c58
OliverAndreasen <oliv2058@stud.kea.dk>
2023-02-09 16:16:02 +0100

    hello

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  FirstApp
-//
-//  Created by Oliver Andreasen on 31/01/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+var body: some View {
+ZStack{
+Image("star")
+.resizable()
+.ignoresSafeArea()
+Rectangle().size(CGSize(width: 61, height: 61.0))
+.padding(100)
+.padding(100)
+.foregroundColor(.white)
+    VStack{
+        Circle()
+                 .fill(Color.pink)
+            .frame(width: 64.0, height: 100)
+                 .shadow(radius: 100)
+             Text("Moon Light")
+                 .font(.title)
+                 .foregroundColor(.yellow)
+                .padding([.top, .leading], 0.0)
+            Text("@Moonlight on all medias").font(.title)
+                .foregroundColor(.yellow)
+               .padding([.top, .leading], 0.0)
+         }
+     }
+ }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
+ }
+ }
+ }
